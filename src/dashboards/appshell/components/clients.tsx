adeeaec87173565fa31a2bda35d24dfa6c9172fc
Ari Milverton <59162894+amilverton@users.noreply.github.com>
2024-01-09 13:33:45 +0800

    avoiding a merge conflict

--- src/dashboards/appshell/components/clients.tsx
+++ src/dashboards/appshell/components/clients.tsx
@@ -1,5 +1,5 @@
 import {metaClientMachine, metaMachine, metaStore,} from "../../../lib/stores/meta/store";
-import {useEffect, useMemo, useRef, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {DBMetaGroup} from "../../../lib/db";
 import {classNames} from "../../../lib/common";
 // @ts-ignore
